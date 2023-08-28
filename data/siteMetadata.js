be9d2a74e7d372dc71d895e9a0282151b98644c5
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2023-08-27 18:22:01 -0700

    Initial customizations

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,19 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: "Ramtin's Blog",
+  author: 'Ramtin Tajbakhsh',
+  headerTitle: 'Ramtin T.',
+  description: 'Personal Blog',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
+  siteUrl: 'https://ramtintjb.com',
+  siteRepo: 'https://github.com/RamtinTJB/personal-blog',
+  siteLogo: '/static/images/logo.jpeg',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'ramtintjb@gmail.com',
+  github: 'https://github.com/RamtinTJB',
+  linkedin: 'https://www.linkedin.com/in/ramtintjb',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
@@ -40,7 +37,6 @@ const siteMetadata = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
   },
   comments: {
     // If you want to use an analytics provider you have to add it to the
