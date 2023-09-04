14397871c62244b763106907e76522c8fb0d2ab5
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2023-09-04 15:23:47 -0700

    Update the projects list

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,31 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Physine',
+    description: `A very simple 2D physics engine written in C++. It uses the SFML library for the graphics.
+    Right now it only supports circular and rectangular colliders. I plan to add more functionality to it
+    in the future.`,
+    //imgSrc: '/static/images/google.png',
+    href: 'https://github.com/RamtinTJB/Physine',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Sudoku Solver',
+    description: `Graphical sudoku solver using C++ and SFML. It uses the backtracking algorithm to solve
+    the sudoku.`,
+    //imgSrc: '/static/images/time-machine.jpg',
+    href: 'https://github.com/RamtinTJB/Sudoku',
+  },
+  {
+    title: 'Terade',
+    description: `A simple terminal based cryptocurrency trading simulator. Nothing technically impressive.
+    It's just the first project that I finished from beginning to end so it holds a special
+    place in my heart :)`,
+    imgSrc: '',
+    href: 'https://github.com/RamtinTJB/Terade'
+  },
+  {
+    title: 'TicTacToe',
+    description: `An unbeatable TicTacToe game using the Minimax algorithm. It is written in C++.`,
+    href: 'https://github.com/RamtinTJB/TicTacToe'
   },
 ]
 
