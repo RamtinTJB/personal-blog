65fb27aaae7405e1f0dd6d8ef83a23dacdbc8fb4
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2024-03-14 14:18:52 -0700

    Add experience section in about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,6 +3,7 @@ import { MDXLayoutRenderer } from 'pliny/mdx-components'
 import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
+import experienceData from '@/data/CV/experienceData'
 
 export const metadata = genPageMetadata({ title: 'About' })
 
@@ -14,6 +15,24 @@ export default function Page() {
     <>
       <AuthorLayout content={mainContent}>
         <MDXLayoutRenderer code={author.body.code} />
+        <h1>Experience</h1>
+        <div className="flex flex-col gap-y-2">
+          {experienceData.map((exp, idx) => (
+            <div className="w-full p-3 border-2 border-teal-500 rounded-md flex gap-5 items-center">
+              <img className="m-0 w-16 rounded-full" src={`/static/images/${exp.logo}`}/>
+              <div className="flex flex-col w-full">
+                <div className="flex justify-between font-bold">
+                  <span>{exp.company}</span>
+                  <span>{exp.date}</span>
+                </div>
+                <div className="flex justify-between italic">
+                  <span>{exp.jobTitle}</span>
+                  <span>{exp.location}</span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </AuthorLayout>
     </>
   )
---
+++ data/CV/experienceData.ts
@@ -0,0 +1,32 @@
+const experienceData = [
+    {
+        company: 'Capital One',
+        date: 'Jun 2024 - Aug 2024',
+        location: 'Plano, TX',
+        jobTitle: 'Software Engineer Intern',
+        logo: 'capital-one-logo.jpeg'
+    },
+    {
+        company: 'Cubic Transportation Systems',
+        date: 'Jun 2023 - Sep 2023',
+        location: 'San Diego, CA',
+        jobTitle: 'Software Engineer Intern',
+        logo: 'cubic-transportation-systems.jpeg'
+    },
+    {
+        company: 'General Atomics / DIII-D',
+        date: 'Jan 2023 - May 2023',
+        location: 'San Diego, CA',
+        jobTitle: 'Software Engineer Intern',
+        logo: 'general-atomics.png'
+    },
+    {
+        company: 'San Diego Mesa College',
+        date: 'Feb 2022 - May 2023',
+        location: 'San Diego, CA',
+        jobTitle: 'Math Peer Mentor',
+        logo: 'mesa-college.png'
+    }
+]
+
+export default experienceData;
